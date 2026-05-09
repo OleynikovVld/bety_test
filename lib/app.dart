38275016f840d7a3911e511f914b3b5bd599f1df
Vladislav Oleynikov <oleynikov.vld@gmail.com>
2026-05-09 20:27:03 +0200

    Add dev comments to App widget

--- lib/app.dart
+++ lib/app.dart
@@ -3,7 +3,12 @@ import 'package:flutter/services.dart';
 
 import 'util/logger.dart';
 
+/// The root widget of the application.
 class App extends StatelessWidget {
+  /// Private constructor to prevent direct instantiation.
+  ///
+  /// The application must be initialized using the [create] factory method
+  /// to ensure all asynchronous dependencies are resolved beforehand.
   const App._();
 
   @override
@@ -16,6 +21,9 @@ class App extends StatelessWidget {
     );
   }
 
+  /// Asynchronous factory method to initialize the application.
+  ///
+  /// Returns a fully initialized [App] widget ready to be passed to [runApp].
   static Future<Widget> create() async {
     WidgetsFlutterBinding.ensureInitialized();
 
